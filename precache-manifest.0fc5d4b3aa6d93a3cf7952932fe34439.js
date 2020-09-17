2efe862ac7668d2d2ab54a6fdff5890801b40758
Suraj Patil <48085134+spielers@users.noreply.github.com>
2020-09-17 13:59:24 +0530

    added

---
+++ precache-manifest.0fc5d4b3aa6d93a3cf7952932fe34439.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "805f71030194131f86d9fc0b93f2660c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "706c0826ef2462cfd77a",
+    "url": "/static/css/main.d769deb8.chunk.css"
+  },
+  {
+    "revision": "73b14983c1b5e690332a",
+    "url": "/static/js/2.19319815.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.19319815.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "706c0826ef2462cfd77a",
+    "url": "/static/js/main.8bcc9fd4.chunk.js"
+  },
+  {
+    "revision": "cb3ccd3eb684c83ffcee",
+    "url": "/static/js/runtime-main.823c8cbe.js"
+  }
+]);
